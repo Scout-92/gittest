e9335dd69feacf73f6f14841001778e53d06d21f
Scout-92 <106506343+Scout-92@users.noreply.github.com>
2022-06-02 15:49:14 +0800

    pull first

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("push github");
+        System.out.println("pull github");
     }
 }
